a313d6ab44b3ffb42721cf1b2e2c43066cca4211
Taki <takiscript@gmail.com>
2025-08-17 05:20:38 +0600

    Add get_inputs() - Basic Usage

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-print("--- WELCOME TO EXPENSE TRACKER ---")
+print("-----------------------------------")
+print(" $$ WELCOME TO EXPENSE TRACKER $$  ")
+print("-----------------------------------")
+
+def get_inputs():
+  user_input: str = input("expense-tracker > ")
+  return user_input
 
 def main():
-  ...
+  while True:
+    user_input = get_inputs()
+    print(user_input)
   
 if __name__ == "__main__":
   main()
