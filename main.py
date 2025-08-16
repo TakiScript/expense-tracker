d98a37ac1328fc19328c8d1823007c8c19444c2b
Taki <takiscript@gmail.com>
2025-08-17 05:36:27 +0600

    Add segment_inputs()

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ print("-----------------------------------")
 print(" $$ WELCOME TO EXPENSE TRACKER $$  ")
 print("-----------------------------------")
 
+def segment_inputs(user_input: str) -> None:
+  segments: list = user_input.lower().strip().split()
+  print(segments)
+
 def get_inputs():
   user_input: str = input("expense-tracker > ")
-  return user_input
+  segment_inputs(user_input)
 
 def main():
   while True:
-    user_input = get_inputs()
-    print(user_input)
+    user_input: str = get_inputs()
   
 if __name__ == "__main__":
   main()
