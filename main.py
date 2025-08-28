d29efa6aefd0aca8d4e371bce2cfde0936a42132
Taki <takiscript@gmail.com>
2025-08-29 04:51:02 +0600

    Add exit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 print("-----------------------------------")
 print(" $$ WELCOME TO EXPENSE TRACKER $$  ")
 print("-----------------------------------")
@@ -8,11 +10,16 @@ def segment_inputs(user_input: str) -> None:
 
 def get_inputs():
   user_input: str = input("expense-tracker > ")
-  segment_inputs(user_input)
+  return user_input
 
 def main():
   while True:
     user_input: str = get_inputs()
+    if user_input in ("0", "exit"):
+      print("Goodbye!~~~")
+      sys.exit()
+    else:
+      segment_inputs(user_input)
   
 if __name__ == "__main__":
   main()
